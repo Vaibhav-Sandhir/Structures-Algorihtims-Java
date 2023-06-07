fd7672f933fe656a8b82b590d0c89b194e6b7290
Vaibhav Sandhir <113308104+Vaibhav-Sandhir@users.noreply.github.com>
2023-06-07 22:44:07 +0530

    Create FirstNonRepeating.java

---
+++ FirstNonRepeating.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class FirstNonRepeating {
+    public static char check(String str){
+        Map<Character,Integer> map = new HashMap<>();
+        for(var item: str.toCharArray()){
+            if(map.containsKey(item)){
+                var count = map.get(item) + 1;
+                map.put(item,count);
+            }
+            else
+                map.put(item,1);
+        }
+        for(var item: str.toCharArray()){
+            if(map.get(item) == 1)
+                return item;
+        }
+        return ' ';
+    }
+}
