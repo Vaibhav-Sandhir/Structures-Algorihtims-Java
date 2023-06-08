2443432d47c8cf15dc7f4fcb9773ea091b1a2aaa
Vaibhav Sandhir <113308104+Vaibhav-Sandhir@users.noreply.github.com>
2023-06-08 13:00:18 +0530

    Create FirstRepeated.java

---
+++ FirstRepeated.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class FirstRepeated {
+    public static char firstRepeated(String str){
+        Map<Character,Integer> map = new HashMap<>();
+        char[] array = str.toCharArray();
+        for(var item: str.toCharArray()){
+            if(map.containsKey(item))
+                return item;
+            else
+                map.put(item,1);
+        }
+        return ' ';
+    }
+}
