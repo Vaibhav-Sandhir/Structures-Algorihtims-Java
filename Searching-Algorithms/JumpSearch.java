b80750304112cfad6c6c43e0a2d18177c0d9b295
Vaibhav Sandhir <113308104+Vaibhav-Sandhir@users.noreply.github.com>
2023-06-27 10:17:31 +0530

    Jump Search Algorithm

---
+++ Searching-Algorithms/JumpSearch.java
@@ -0,0 +1,20 @@
+public class JumpSearch {
+    public static int search(int[] array,int target){
+        int blockSize = (int)Math.sqrt(array.length);
+        int start = 0;
+        int next = blockSize;
+
+        while(start < array.length && array[next - 1] < target){
+            start = next;
+            next += blockSize;
+            if(next > array.length)
+                next = array.length;
+        }
+
+        for(int i = start; i < next; i++){
+            if(array[i] == target)
+                return i;
+        }
+        return -1;
+    }
+}
